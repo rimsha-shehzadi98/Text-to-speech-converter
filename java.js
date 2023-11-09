d053a55288872e46f9c250fcd744c1ac9538a005
rimsha-shehzadi98 <rimshashehazdi98@gmail.com>
2023-11-09 21:02:56 +0500

    Updated

--- java.js
+++ java.js
@@ -1,21 +1,24 @@
 let speech = new SpeechSynthesisUtterance();
-
 let voices = [];
-
 let voiceSelect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged = () =>{
+window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
 
-    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+    if (voices.length > 0) {
+        speech.voice = voices[0];
+
+        voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+    }
 };
 
-voiceSelect.addEventListener("change", () =>{
-    speech.voice = voices[voiceSelect.value];
+voiceSelect.addEventListener("change", () => {
+    if (voices.length > voiceSelect.value) {
+        speech.voice = voices[voiceSelect.value];
+    }
 });
 
-document.querySelector("button").addEventListener("click" , ()=>{
+document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
